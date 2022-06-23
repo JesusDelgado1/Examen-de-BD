c2cf2ebd5867b698210360b0ecf3d1dbf69e6228
JesusDelgado1 <jesusdelgado20042001@gmail.com>
2022-06-23 05:57:48 +0000

    Creacion de multiples tablas

---
+++ Examen.sql
@@ -0,0 +1,9 @@
+Create table Región(
+    id_region       NUMBER(1000) NOT NULL,
+    nombre          VARCHAR(254) NOT NULL,
+);
+
+Create Table Provincia(
+    id_provincia    NUMBER(1000) NOT NULL,
+    nombre          VARCHAR(254) NOT NULL,
+);
